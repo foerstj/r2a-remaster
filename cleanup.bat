df93ee94c811dbb66b9011fae976aff52c531438
Johannes Förstner <johannes@famfoerstner.de>
2025-03-02 14:48:03 +0100

    Cleanup map as well

--- cleanup.bat
+++ cleanup.bat
@@ -4,7 +4,6 @@ set map_cs=Return to Arhok
 set ds=%DungeonSiege%
 
 :: Cleanup resources so as not to confuse Siege Editor
+del "%ds%\DSLOA\%map_cs%.dsmap"
 del "%ds%\DSLOA\%map_cs%.dsres"
-del "%ds%\DSLOA\%map_cs%.de.dsres"
-del "%ds%\DSLOA\%map_cs%.es.dsres"
-del "%ds%\DSLOA\%map_cs%.fr.dsres"
+del "%ds%\DSLOA\%map_cs%.*.dsres"
