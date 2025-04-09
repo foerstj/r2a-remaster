8274ae8bf671499f4358c898ff83bce6e3f7e91c
Johannes Förstner <johannes@famfoerstner.de>
2025-04-09 21:25:07 +0200

    Exclude also RawToPsd.exe and its outputs

--- build.bat
+++ build.bat
@@ -48,7 +48,7 @@ if %errorlevel% neq 0 pause
 
 :: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\art" "%tmp%\Bits\art" /E /xf *.lqd20
+robocopy "%bits%\art" "%tmp%\Bits\art" /E /xf *.lqd20 /xf *.exe /xf *.psd
 robocopy "%bits%\sound" "%tmp%\Bits\sound" /E
 robocopy "%bits%\world\ai" "%tmp%\Bits\world\ai" /E
 robocopy "%bits%\world\contentdb" "%tmp%\Bits\world\contentdb" /E
